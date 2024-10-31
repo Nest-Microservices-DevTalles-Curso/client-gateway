7ebd7fc1e243d585bd175f4c3533154744f3cdad
Kevin Espinoza <kevinespinozadev@gmail.com>
2024-10-30 23:24:41 -0300

    Full authentication, guards, remake token jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
 import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -22,13 +25,19 @@ export class AuthController {
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
-    return this.client.send('auth.login.user', loginUserDto);
+    return this.client.send('auth.login.user', loginUserDto)
+    .pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      })
+    );
 
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verifyToken() {
-    return this.client.send('auth.verify.user', {});
-
+  verifyToken( @User() user: CurrentUser, @Token() token: string ) {
+    //return this.client.send('auth.verify.user', {});
+    return {user, token}
   }
 }
